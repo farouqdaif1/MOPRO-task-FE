b9f1b234c0454b7da3e6b816cf9b24f53b587646
farouqdaif1 <daiffarouq@gmail.com>
2025-03-04 05:59:37 +0200

    first commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,12 +7,12 @@ export const fetchDevices = async () => {
     return response.data;
 };
 
-export const fetchDeviceById = async (id) => {
-    const response = await axios.get(`${API_BASE_URL}/devices/${id}`);
-    return response.data;
-};
+// export const fetchDeviceById = async (id) => {
+//     const response = await axios.get(`${API_BASE_URL}/devices/${id}`);
+//     return response.data;
+// };
 
-export const updateDevice = async (id, updatedData) => {
-    const response = await axios.put(`${API_BASE_URL}/devices/${id}`, updatedData);
-    return response.data;
-};
+// export const updateDevice = async (id, updatedData) => {
+//     const response = await axios.put(`${API_BASE_URL}/devices/${id}`, updatedData);
+//     return response.data;
+// };
